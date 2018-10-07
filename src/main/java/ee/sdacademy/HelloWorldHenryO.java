07cbf59c50193f4e1fbc0d7cffad4243cd607819
Henry Man <henry@man.com>
2018-10-07 10:52:41 +0300

    Println message added

--- src/main/java/ee/sdacademy/HelloWorldHenryO.java
+++ src/main/java/ee/sdacademy/HelloWorldHenryO.java
@@ -3,5 +3,9 @@ package ee.sdacademy;
 public class HelloWorldHenryO {
     public static void main(String[] args) {
 
+        String a = "Man";
+        String b = "United";
+
+        System.out.println(a + " "+ b);
     }
 }
