1a011b155eb08592e08be903ed74baf7062da8aa
Artjom Prozorov <timprozorov@icloud.com>
2018-10-07 10:50:23 +0300

    New class created (Name added)

---
+++ src/main/java/ee/sdacademy/HelloWorldArtjom.java
@@ -0,0 +1,13 @@
+package ee.sdacademy;
+
+public class HelloWorldArtjom {
+
+    public static void main(String[] args) {
+
+
+        String s = "Artjom";
+        String b = "Prozorov";
+
+        System.out.println (s+" "+b);
+    }
+}
