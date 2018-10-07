53b1473cb81d97a57b12ab1d83956a98a7b41b11
Andre Kose <naabrimees888@gmail.com>
2018-10-07 14:25:47 +0300

    Second commit renamed

--- src/main/java/ee/sdacademy/HelloWorld.java
+++ src/main/java/ee/sdacademy/HelloWorld.java
@@ -3,6 +3,6 @@ package ee.sdacademy;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World Nishant");
+        System.out.println("Andre is learning GIT");
     }
 }
