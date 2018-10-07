932eb70d75e29a1997d042425813fc4c2acba8db
Gabor1977 <kisgyarisz@gmail.com>
2018-10-07 14:17:32 +0300

    MaxNew

--- src/main/java/ee/sdacademy/MaxGabor.java
+++ src/main/java/ee/sdacademy/MaxGabor.java
@@ -3,16 +3,7 @@ package ee.sdacademy;
 public class MaxGabor {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-
-        int max;
-        if (a > b) {
-            max = a;
-        } else {
-            max = b;
-        }
-        System.out.println("Max value is: " + max);
+        System.out.println("New Max file copied");
 
     }
 }
