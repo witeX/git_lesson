9d4b9e80df65f61f28e8d5dd7ba76879e14a4849
Gabor1977 <kisgyarisz@gmail.com>
2018-10-07 14:46:44 +0300

    Change after the revert test.

--- src/main/java/ee/sdacademy/MaxGabor.java
+++ src/main/java/ee/sdacademy/MaxGabor.java
@@ -3,7 +3,8 @@ package ee.sdacademy;
 public class MaxGabor {
 
     public static void main(String[] args) {
-        System.out.println("New Max file copied");
+
+        System.out.println("Second update on this file");
 
     }
 }
