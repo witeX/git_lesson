48b99293192365de24eae1e8b963646178db401f
chrisricolang <chrisrico.lang@gmail.com>
2018-10-07 11:26:47 +0300

    My first commit to the GIT repo

--- src/main/java/ee/sdacademy/Main.java
+++ src/main/java/ee/sdacademy/Main.java
@@ -2,7 +2,5 @@ package ee.sdacademy;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world for first git issue.");
-    }
+    public static void main(String[] args) { System.out.println("Hello world for first git issue."); }
 }
