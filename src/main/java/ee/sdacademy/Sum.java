630e5e09ddbb2928bdf30207fa7dc5cf940b9e19
Aleksandr Beljakov <aleksandr.beljakov@insparx.com>
2018-10-07 09:28:53 +0300

    We need to calculate sum of 10 elements from array and print this sum to terminal #9;

---
+++ src/main/java/ee/sdacademy/Sum.java
@@ -0,0 +1,17 @@
+package ee.sdacademy;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        final int[] array = new int[]{
+                32, 4, 4, 223, 123, 223, 55, 123, 55, 223
+        };
+
+        int sum = 0;
+        for (int anArray1 : array) {
+            sum += anArray1;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
