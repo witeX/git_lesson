cc8efaedbdbe85b82912e014b2685de727ee5f29
Artjom Prozorov <timprozorov@icloud.com>
2018-10-07 14:27:21 +0300

    Change my commit

--- src/main/java/ee/sdacademy/HelloWorldArtjom.java
+++ src/main/java/ee/sdacademy/HelloWorldArtjom.java
@@ -4,13 +4,10 @@ public class HelloWorldArtjom {
 
     public static void main(String[] args) {
 
-
         String s = "Artjom";
         String b = "Prozorov";
 
-
-        System.out.println (s + " " + b);
-        System.out.println (s + " " + b);
+        System.out.println (s+" "+b+" "+ "is learning git");
 
     }
 
