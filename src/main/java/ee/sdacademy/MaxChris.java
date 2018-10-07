8a6a2eda290b22fc32d0b79c3c45a5b6d623cce1
chrisricolang <chrisrico.lang@gmail.com>
2018-10-07 14:22:12 +0300

    Committing MaxChris.java & HelloWorldChris.java

---
+++ src/main/java/ee/sdacademy/MaxChris.java
@@ -0,0 +1,18 @@
+package ee.sdacademy;
+
+public class MaxChris {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        int max;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+        System.out.println("Max value is: " + max);
+
+    }
+}
